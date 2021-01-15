4d0e2617485b8945dee09ab30e4f6b6cc239be12
ORG4N <callumorgan@gmail.com>
2021-01-15 00:29:08 +0000

    Webapp animations and reservations page front end design complete

--- doki-doki-delight-management-system/wwwroot/js/scripts.js
+++ doki-doki-delight-management-system/wwwroot/js/scripts.js
@@ -1,13 +1,50 @@
 $(document).ready(function () {
-    $(document).on("click","#continueButton",function(){
-        $('main').load('second.html');
-    })
 
-    $(document).on("click","#customerButton",function(){
-        $('main').load('reservation.html');
-    })
 
-    $(document).on("click","#employeeButton",function(){
-        $('main').load('signIn.html');
-    })
+    // If user clicks the frontpage continue button take them to user selection
+    $(document).one("click","#continueButton",function(){
+
+        const element = document.querySelector('#index');
+        element.classList.add('animate__animated','animate__slideOutRight');           // add an animation to the index div
+
+        element.addEventListener('animationend', () => {                                // wait until animation ends and load new content
+            console.log('Animation ended');
+            loadDoc('second');
+        });
+    });
+
+
+    // If user clicks the button to say theyre a customer, take them to reservation booking page
+    $(document).one("click","#customerButton",function(){
+
+        const element = document.querySelector('#userselect');
+        element.classList.add('animate__animated','animate__slideOutRight');
+
+        element.addEventListener('animationend', () => {
+            console.log('Animation ended');
+            loadDoc('reservation');
+        });
+    });
+
+    // If user clicks the button to say theyre an employee, take them to the sign in page
+    $(document).one("click","#employeeButton",function(){
+
+        const element = document.querySelector('#userselect');
+        element.classList.add('animate__animated','animate__slideOutRight');
+
+        lelement.addEventListener('animationend', () => {
+            console.log('Animation ended');
+            loadDoc('signIn');
+        });
+    });
 });
+
+// Function for loading different html files
+function loadDoc(page){
+
+    if (page=='second')     {$('main').load('second.html');}        // Load the User Select page
+
+    else if (page=='reservation'){$('main').load('reservation.html');}   // Load the Reservation page
+
+    else if (page=='signIn')     {$('main').load('signIn.html');}        // Load the Employee Sign In page
+}
