3462bd0254eced0e98bef02f15e618f3b8bb1b12
ORG4N <callumorgan@gmail.com>
2021-01-17 03:15:41 +0000

    Added simplistic sidebar for simple navigation around webapp. Icons use feathericons collection

--- doki-doki-delight-management-system/wwwroot/js/scripts.js
+++ doki-doki-delight-management-system/wwwroot/js/scripts.js
@@ -1,50 +1,75 @@
 $(document).ready(function () {
 
+    // If user clicks the sidebar home button, send back to front page
+    $(document).on("click","#homeButton",function(){
 
-    // If user clicks the frontpage continue button take them to user selection
-    $(document).one("click","#continueButton",function(){
-
-        const element = document.querySelector('#index');
+        const element = document.querySelector('#page');
         element.classList.add('animate__animated','animate__slideOutRight');           // add an animation to the index div
 
         element.addEventListener('animationend', () => {                                // wait until animation ends and load new content
-            console.log('Animation ended');
+            loadDoc('home');
+        });
+    });
+
+
+    // If user clicks the frontpage continue button take them to user selection
+    $(document).on("click","#continueButton",function(){
+
+        const element = document.querySelector('#page');
+        element.classList.add('animate__animated','animate__slideOutRight');           // add an animation to the index div
+
+        element.addEventListener('animationend', () => {                                // wait until animation ends and load new content
             loadDoc('userselect');
         });
     });
 
 
     // If user clicks the button to say theyre a customer, take them to reservation booking page
-    $(document).one("click","#customerButton",function(){
+    $(document).on("click","#customerButton",function(){
 
-        const element = document.querySelector('#userselect');
+        const element = document.querySelector('#page');
         element.classList.add('animate__animated','animate__slideOutRight');
 
         element.addEventListener('animationend', () => {
-            console.log('Animation ended');
             loadDoc('reservation');
         });
     });
 
     // If user clicks the button to say theyre an employee, take them to the sign in page
-    $(document).one("click","#employeeButton",function(){
+    $(document).on("click","#employeeButton",function(){
 
-        const element = document.querySelector('#userselect');
+        const element = document.querySelector('#page');
         element.classList.add('animate__animated','animate__slideOutRight');
 
         element.addEventListener('animationend', () => {
-            console.log('Animation ended');
             loadDoc('signIn');
+
         });
     });
+
+    $(document).on("click","#clearButton",function(){
+
+        document.querySelector('form').reset();
+
+    });
+
+    $(document).on("click","#reservationButton",function(){
+
+        document.querySelector('form').submit();
+
+    });
+
+
 });
 
 // Function for loading different html files
 function loadDoc(page){
 
-    if (page=='userselect')     {$('main').load('userselect.html');}        // Load the User Select page
+    if (page=='home'){$('main').load('index.html');}        // Load the User Select page
+
+    else if (page=='userselect'){$('main').load('userselect.html');}        // Load the User Select page
 
     else if (page=='reservation'){$('main').load('reservation.html');}   // Load the Reservation page
 
-    else if (page=='signIn')     {$('main').load('signIn.html');}        // Load the Employee Sign In page
+    else if (page=='signIn'){$('main').load('signIn.html');}        // Load the Employee Sign In page
 }
